dbdd47f36feb33d237cce6ee45aa77230e7dad31
Daniel Lugo <daniellugofreelancer@gmail.com>
2023-03-30 11:18:18 -0300

    update axios to get postlist

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,19 +1,29 @@
 import React, { useEffect, useState } from 'react'
 import Post from './Post'
+import axios from 'axios';
 
 export default function PostList() {
   const [financialPosts, setFinancialPosts] = useState([])
   useEffect(() => {
    
 
-    fetch('https://exuberant-pink-angelfish.cyclic.app/post').then((response)=>{
-    response.json().then((posts)=>{
-      console.log(posts)
-      setFinancialPosts(posts)
-    })
-    }).catch((error)=>{
-      console.log(error)
-    })
+    // fetch('https://exuberant-pink-angelfish.cyclic.app/post').then((response)=>{
+    // response.json().then((posts)=>{
+    //   console.log(posts)
+    //   setFinancialPosts(posts)
+    // })
+    // }).catch((error)=>{
+    //   console.log(error)
+    // })
+
+    axios.get('https://exuberant-pink-angelfish.cyclic.app/post')
+  .then((response) => {
+    console.log(response.data);
+    setFinancialPosts(response.data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
   
 
   }, [])
